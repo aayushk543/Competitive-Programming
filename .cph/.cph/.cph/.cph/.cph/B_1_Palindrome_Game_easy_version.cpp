7932a317baed57e1e632a5233e3b7f92a9224f4d
Aayush Kushwaha <aayush2005k@gmail.com>
2026-06-15 02:12:56 +0530

    14/06/2026

--- .cph/.cph/.cph/.cph/.cph/B_1_Palindrome_Game_easy_version.cpp
+++ .cph/.cph/.cph/.cph/.cph/B_1_Palindrome_Game_easy_version.cpp
@@ -17,16 +17,13 @@ int main() {
 
         int n_zero = 0;
 
-        for(int i = 0; i < n; i++){
-            if(s[i] == '0') n_zero++;
+        for(int i = 0; i < s.length(); i++){
+            n_zero += '1' - s[i];
         }
-        
-        if(n_zero % 2 == 1){
-            n_zero -= 1;
-            if(n_zero % 4 == 0) cout << "BOB" << '\n';
-            else cout << "ALICE" << '\n';
+        if(n_zero == 1) cout << "BOB" << '\n';
+        else if(n_zero % 2 == 1){
+            cout << "ALICE" << '\n';
         }
-        else if(n_zero % 4 == 0) cout << "DRAW" << '\n';
         else cout << "BOB" << '\n';
     }
     return 0;
